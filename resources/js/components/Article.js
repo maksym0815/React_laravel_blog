3d1d1b835f0a25050b9bcdccaf445635ece68cb4
maksymprokhnitskyi <maksymprokhnitskyi@gmail.com>
2021-01-11 16:46:21 -0500

    added like button with toogle

--- resources/js/components/Article.js
+++ resources/js/components/Article.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeartBroken } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Article = ({ article }) => {
     const { title, content, image_url, updated_at, slug } = article;
+    const [like, setLike] = useState(false);
+    const toggle = () => {
+        setLike(!like);
+    };
+    const handleLike = () => {};
     return (
         <div className="card mb-3">
             <img className="card-img-top" src={image_url} alt={slug} />
@@ -11,6 +19,23 @@ const Article = ({ article }) => {
                 <p className="card-text">
                     <small className="text-muted">{updated_at}</small>
                 </p>
+            </div>
+            <div className="card-footer">
+                <div className="container">
+                    <center>
+                        <div
+                            className="container"
+                            style={{ border: "1px solid black", width: "15%" }}
+                            onClick={toggle}
+                        >
+                            {!like ? (
+                                <FontAwesomeIcon icon={faHeart} />
+                            ) : (
+                                <FontAwesomeIcon icon={faHeartBroken} />
+                            )}
+                        </div>
+                    </center>
+                </div>
             </div>
         </div>
     );
